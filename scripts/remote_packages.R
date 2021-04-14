bd5d1b3880a6e8482f30f8e1ec1ee0ab683ff57c
Thibaut Jombart <thibautjombart@gmail.com>
2021-04-14 16:48:15 +0200

    trying to use the envir var token

--- scripts/remote_packages.R
+++ scripts/remote_packages.R
@@ -1,14 +1,24 @@
 
-## Use this script to install non-cran packages; note that the default `force =
-## FALSE` means installs are only performed if the current version is not up to
-## date (or not the right tag).
+# Use this script to install non-cran packages; note that the default `force =
+# FALSE` means installs are only performed if the current version is not up to
+# date (or not the right tag).
+
+
+# Here we handle the phifunc install token as follows, by order of decreasing
+# priority:
+
+## 1. Grab the token from a local 'phifunc_token' file if it exists
+## 2. Grab the token the environment variable 'PHIFUNC_TOKEN'
+## 3. If the above is empty, set the value to NULL
 
 phifunc_token_file <- here::here("phifunc_token")
 if (file.exists(phifunc_token_file)) {
   phifunc_token <- scan(phifunc_token_file, what = "character")
 } else {
-  phifunc_token <- NULL
+  phifunc_token <- Sys.getenv("PHIFUNC_TOKEN")
+  if (phifunc_token == "") phifunc_token <- NULL
 }
+
 
 remotes::install_github("reconhub/trending@bootstrap", upgrade = "never")
 remotes::install_github("reconverse/incidence2", upgrade = "never")
