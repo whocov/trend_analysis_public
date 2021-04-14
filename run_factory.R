e2e165b6da75a5bc0f50b7383640145e9cf7ffa9
Thibaut Jombart <thibautjombart@gmail.com>
2021-04-14 15:41:35 +0200

    adding asmodee outputs for the day

--- run_factory.R
+++ run_factory.R
@@ -5,8 +5,8 @@ library(reportfactory)
 compile_reports(report = "assemble", params = list(import_data = TRUE))
 
 ## Some of these regions error, need to check why manually
-## regions <- c("AFRO", "EMRO", "EURO", "PAHO", "SEARO", "WPRO")
-regions <- c("AFRO", "EURO", "PAHO", "SEARO", "WPRO")
+regions <- c("AFRO", "EMRO", "EURO", "PAHO", "SEARO", "WPRO")
+## regions <- c("AFRO", "EURO", "PAHO", "SEARO", "WPRO")
 
 
 ## Note: n_cores needs to be adjusted by the user; best removed on windows,
