5bf4a832f602b7d7656faf943fd11e733e1279b3
Thibaut Jombart <thibautjombart@gmail.com>
2021-04-15 15:09:37 +0200

    WHO region as optional envir var

--- run_remote_trendbreaker.R
+++ run_remote_trendbreaker.R
@@ -1,8 +1,19 @@
 # translate the Rmd to R
 knitr::purl(here::here("report_sources", "trendbreaker.Rmd"))
 
-# run report for each region
-regions <- c("AFRO", "EMRO", "EURO", "PAHO", "SEARO", "WPRO")
+# run report for each region, or a single region if WHO_REGION is a specified
+# environment variable; the latter is useful to have different actions compile
+# different regions
+
+regions <- Sys.getenv("WHO_REGION")
+if (regions == "") {
+  regions <- c("AFRO", "EMRO", "EURO", "PAHO", "SEARO", "WPRO")
+} else {
+  regions <- toupper(regions)
+  expected_regions <- c("AFRO", "EMRO", "EURO", "PAHO", "SEARO", "WPRO")
+  regions <- match.arg(regions, expected_regions)
+}
+
 for (reg in regions) {
   params <- list(who_region = reg)
   source("trendbreaker.R")
