6ac7d3454033b2a24191946141a9e4bc439c5e55
Thibaut Jombart <thibautjombart@gmail.com>
2021-04-19 11:15:17 +0200

    adding commented out no failsafe

--- run_factory.R
+++ run_factory.R
@@ -1,19 +1,23 @@
 
 library(reportfactory)
 
-## Update data
+# Update data
 compile_reports(report = "assemble", params = list(import_data = TRUE))
 
-## Some of these regions error, need to check why manually
+# Some of these regions error, need to check why manually
 regions <- c("AFRO", "EMRO", "EURO", "PAHO", "SEARO", "WPRO")
 ## regions <- c("AFRO", "EURO", "PAHO", "SEARO", "WPRO")
 
 
-## Note: n_cores needs to be adjusted by the user; best removed on windows,
-## which does not support this parallelisation.
+# Note: n_cores needs to be adjusted by the user; best removed on windows,
+# which does not support this parallelisation.
 for (reg in regions) {
   try(compile_reports(report = "trendbreaker.Rmd",
                   params = list(who_region = reg, n_cores = 12),
                   subfolder = reg),
       silent = TRUE)
+  ## version with no failsafe
+  ## compile_reports(report = "trendbreaker.Rmd",
+  ##                 params = list(who_region = reg, n_cores = 12),
+  ##                 subfolder = reg)
 }
