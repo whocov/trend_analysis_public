f85820313cdae909528cd7056781f25ed7fffbfb
Thibaut Jombart <thibautjombart@gmail.com>
2021-06-10 10:59:43 +0200

    changing body

--- run_synthesis.R
+++ run_synthesis.R
@@ -19,7 +19,7 @@ library(magrittr)
 recipients <- c("thibautjombart@gmail.com", "polaino@who.int", "batran@who.int", "laurensonschaferh@who.int")
 
 txt_body <- sprintf(
-  "Please find attached the latest COVID-19 dynamics synthesis report. This automated update was generated at %s. You can download the full .xlsx file from: https://github.com/whocov/trend_analysis_public/raw/main/synthesis/dynamics_synthesis_latest.xlsx",
+  "Dear Colleague, <br><br>Please find attached the latest COVID-19 dynamics synthesis report as well as an xlsx file with pre-classification of countries sorted by WHO region. Note that not all countries may have been included in the analysis, so that some lines may be empty. You will find more information on inclusion criteria at: https://github.com/whocov/trend_analysis_public/.<br><br>This automated update was generated on the %s.<br><br>Best regards, <br>Thibaut Jombart (through github actions)",
   Sys.time()
   )
 
